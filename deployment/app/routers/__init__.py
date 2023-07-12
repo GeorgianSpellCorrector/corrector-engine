6c63443c99ced4901a2af944b29d0f41b0c3d6fd
ZurabDz <zuradzindzibadze06@gmail.com>
2023-07-12 08:54:24 +0400

    inferencing

---
+++ deployment/app/routers/__init__.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+from routers import inference
+
+api = APIRouter()
+
+api.include_router(inference.router, prefix='/inference', tags=['inference'])
---
+++ deployment/app/routers/inference.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from models.serializers import CorrectorRequest, CorrectedResult
+from routers.utils import inference
+
+router = APIRouter()
+
+
+@router.post('/correct_sentences')
+async def correct_sentences(request: CorrectorRequest):
+    corrected = inference(request)
+
+    return corrected
