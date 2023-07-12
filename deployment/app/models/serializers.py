cc179e2bf7e5d7e8103af8f6288256077e8bf3f4
ZurabDz <zuradzindzibadze06@gmail.com>
2023-07-12 08:53:56 +0400

    serilizing requests and response

---
+++ deployment/app/models/serializers.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import List, Dict
+
+
+class CorrectorRequest(BaseModel):
+    headers: Dict[str, str]
+    texts: List[str]
+
+class CorrectedResult(BaseModel):
+    result: Dict[str, str]
